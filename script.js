84b58c845bdbc32255a32d48784147d604e50ab7
Ajver <zawlockidominik@gmail.com>
2018-10-25 21:50:59 +0200

    zoom

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 var cells = [];
 var col, row;
 
+var zoom = 1.0;
+
 function setup() {
   createCanvas(500, 500);
   
-  col = width / cw;
-  row = height / cw;
+  col = width / cw * 2;
+  row = width / cw * 2;
   
   for(let i=0; i<100; i++) {
     let color = [];
@@ -14,9 +16,8 @@ function setup() {
     color[1] = random(256);
     color[2] = random(256);
     
-    let x = floor(random(col))*cw;
-    let y = floor(random(row))*cw;
-    console.log("xy: " + x + ' ' + y);
+    let x = floor(random(col))*cw - width;
+    let y = floor(random(row))*cw - height;
     let cell = new Cell(x, y, color);
     cells.push(cell);
   }
@@ -24,9 +25,19 @@ function setup() {
 }
 
 function draw() {
+  translate(width/2, height/2);
+  scale(zoom);
   background(0);
   
   for(let i=0; i<cells.length; i++) {
     cells[i].draw();
+  }
+}
+
+function keyPressed() {
+  if(keyCode == 187) {
+    zoom *= 1.05;
+  }else if(keyCode == 189) {
+    zoom *= 0.95;
   }
 }
